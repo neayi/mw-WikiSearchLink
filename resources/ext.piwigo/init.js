9e8f19fceb5de004a58d887e379f7ab20b505f28
Bertrand Gorge <bertrand.gorge@neayi.com>
2022-08-24 18:42:53 +0200

    Completed the extension. Added a category (album) parameter as well as a count parameter. The parser function now works as well

--- resources/ext.piwigo/init.js
+++ resources/ext.piwigo/init.js
@@ -9,49 +9,72 @@
 		init: function () {
 			var self = this;
 
-            console.log("Piwigo Hello world");
-
             $('.showPiwigo').each(function () {
                 var piwigoDiv = $(this);
 
+                // NB : Tags will take over category (album)
                 var tags = piwigoDiv.data( "tags" );
+                var tags_multiple = piwigoDiv.data( "tags_multiple" );
+                var category = piwigoDiv.data( "category" );
+                var count = piwigoDiv.data( "count" );
 
-                self.getImages(tags, piwigoDiv);
-
-
+                self.getImages(tags, tags_multiple, category, count, piwigoDiv);
             });
 
 		},
 
-        getImages: function (tags, piwigoDiv) {
+        getImages: function (tags, tags_multiple, category, count, piwigoDiv) {
 
-            //            var piwigoURL = mw.config.get('NeayiInteractions').wgInsightsRootURL;
-            var piwigoURL = 'https://galerie.dev.tripleperformance.fr/ws.php';
+            var piwigoRootURL = mw.config.get('Piwigo').wgPiwigoURL;
+            var piwigoWSURL = piwigoRootURL + "/ws.php?format=json";
+            var piwigoURL = piwigoRootURL + '/?/';
+
+            if (tags !== undefined)
+            {
+                piwigoWSURL = piwigoWSURL + "&method=pwg.tags.getImages&tag_id=" + tags;
+                piwigoURL = piwigoURL + 'tags/' + tags;
+            }
+            else if (tags_multiple !== undefined)
+            {
+                piwigoWSURL = piwigoWSURL + "&method=pwg.tags.getImages&tag_id[]=" + tags_multiple.split(',').join("&tag_id[]=");
+                piwigoURL = piwigoURL + 'tags/' + tags_multiple.split(',').at(0); // Target the first tag only
+            }
+            else if (category !== undefined)
+            {
+                piwigoWSURL = piwigoWSURL + "&method=pwg.categories.getImages&cat_id=" + category;
+                piwigoURL = piwigoURL + 'category/' + category;
+            }
+
+            if (count > 0)
+                piwigoWSURL = piwigoWSURL + "&per_page=" + count;
+
+            // Add a button with the URL to the gallery:
+            $(`<div class="text-right">
+                    <a  type="button" class="btn btn-primary btn-sm text-white" href="${piwigoURL}" target="_blank">Voir la galerie</a>
+                </div><br style="clear:both"/>`).insertAfter(piwigoDiv);
 
             $.ajax({
-                    url: piwigoURL + "?format=json&method=pwg.tags.getImages&tag_id=" + tags,
+                    url: piwigoWSURL,
                     dataType: 'json',
                     method: "GET",
             }).done(function (data) {
 
-                    console.log(data);
+                var rowDiv = $('<div>').attr('class', 'row');
+                data.result.images.forEach(item => {
 
-                    var rowDiv = $('<div>').attr('class', 'row');
-                    data.result.images.forEach(item => {
+                    var large = item.element_url;
+                    var thumb = item.derivatives.small.url;
 
-                        var large = item.element_url;
-                        var thumb = item.derivatives.small.url;
+                    rowDiv.append($(`<div class="col-sm-12 col-md-4">
+                        <a class="lightbox" href="${large}">
+                            <img src="${thumb}" alt="Bridge">
+                        </a>
+                    </div>`));
+                });
 
-                        rowDiv.append($(`<div class="col-sm-12 col-md-4">
-                            <a class="lightbox" href="${large}">
-                                <img src="${thumb}" alt="Bridge">
-                            </a>
-                        </div>`));
-                    });
+                piwigoDiv.append(rowDiv);
 
-                    piwigoDiv.append(rowDiv);
-
-                    baguetteBox.run('.showPiwigo');
+                baguetteBox.run('.showPiwigo');
             });
 
         }
--- src/Hooks.php
+++ src/Hooks.php
@@ -24,25 +24,8 @@ use Parser;
 use PPFrame;
 
 class Hooks implements
-	\MediaWiki\Hook\BeforePageDisplayHook,
 	\MediaWiki\Hook\ParserFirstCallInitHook
 {
-
-	/**
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/BeforePageDisplay
-	 * @param \OutputPage $out
-	 * @param \Skin $skin
-	 */
-	public function onBeforePageDisplay( $out, $skin ): void {
-		$config = $out->getConfig();
-
-
-		if ( $config->get( 'PiwigoURL' ) ) {
-			$out->addModules( 'oojs-ui-core' );
-			$out->addHTML( \Html::element( 'p', [], 'Piwigo was here: ' . $config->get( 'PiwigoURL' ) ) );
-		}
-	}
-
 	/**
 	 * Register parser hooks to add the piwigo keyword
 	 *
@@ -54,12 +37,11 @@ class Hooks implements
 	public function onParserFirstCallInit( $parser ) {
 
 		// Add the following to a wiki page to see how it works:
-		// <piwigo foo="bar" baz="quux" />
+		// <piwigo tags=1 count=5 /> or <piwigo category=1 count=5 />
 		$parser->setHook( 'piwigo', [ self::class, 'parserKeywordPiwigo' ] );
 
-
 		// Add the following to a wiki page to see how it works:
-		// {{#piwigo: hello | hi | there }}
+		// {{#piwigo: tags=1 | tags=2 | count = 10 }}
 		$parser->setFunctionHook( 'piwigo', [ self::class, 'parserFunctionPiwigo' ] );
 
 		return true;
@@ -75,21 +57,22 @@ class Hooks implements
 	 * @param PPFrame $frame the parent frame
 	 * @return string to replace tag with
 	 */
-	public static function parserKeywordPiwigo(
-		$input,
-		array $args,
-		Parser $parser,
-		PPFrame $frame
-	) {
+	public static function parserKeywordPiwigo( $input, array $args, Parser $parser, PPFrame $frame ) {
 		$parser->getOutput()->addModules( 'ext.piwigo' );
 		$parser->getOutput()->addModules( 'ext.baguetteBox' );
 
-		$ret = '<div>This is the gallery</div>';
-
-		if ( isset( $args['tags'] ) ) {
-			$ret = '<div>This is the gallery for tag: '.$args['tags'].'</div><div class="showPiwigo tz-gallery" data-tags="'.$args['tags'].'"></div>';
+		if (empty($GLOBALS['wgPiwigoURL']))
+		{
+			return '<p>Please add <code>$wgPiwigoURL</code> to your LocalSettings.php</p>';
 		}
 
+		$piwigoParams = [];
+		$piwigoParams[ 'wgPiwigoURL' ] = $GLOBALS['wgPiwigoURL'];
+        $piwigoParams[ 'wgPiwigoGalleryLayout' ] = $GLOBALS['wgPiwigoGalleryLayout'] ?? 'fluid';
+
+		$parser->getOutput()->addJsConfigVars( 'Piwigo', $piwigoParams );
+
+		$ret = self::getGalleryTag($args);
 
 		return $ret;
 	}
@@ -107,13 +90,78 @@ class Hooks implements
 		$parser->getOutput()->addModules( 'ext.piwigo' );
 		$parser->getOutput()->addModules( 'ext.baguetteBox' );
 
-		$piwigo = [
-			'value' => $value,
-			'arguments' => $args,
-		];
+		$args[] = $value;
 
-		return '<pre>piwigo Function: '
-			. htmlspecialchars( FormatJson::encode( $piwigo, /*prettyPrint=*/true ) )
-			. '</pre>';
+		// Format the parameters in a nice array
+		$parameters = array();
+		foreach ($args as $argpair)
+		{
+			$parts = array();
+			if (preg_match('@^([^=]+)=?(.*)$@', $argpair, $parts))
+			{
+				$k = strtolower(trim($parts[1]));
+				$value = trim(preg_replace('@-.*$@', '', $parts[2]));
+
+				if (isset($parameters[$k]))
+				{
+					if (!is_array($parameters[$k]))
+					{
+						$v = $parameters[$k];
+						unset($parameters[$k]);
+
+						$parameters[$k][] = $v;
+					}
+
+					$parameters[$k][] = $value;
+				}
+				else
+					$parameters[$k] = $value;
+			}
+		}
+
+		$parser->getOutput()->addModules( 'ext.piwigo' );
+		$parser->getOutput()->addModules( 'ext.baguetteBox' );
+
+		if (empty($GLOBALS['wgPiwigoURL']))
+		{
+			return '<p>Please add <code>$wgPiwigoURL</code> to your LocalSettings.php</p>';
+		}
+
+		$piwigoParams = [];
+		$piwigoParams[ 'wgPiwigoURL' ] = $GLOBALS['wgPiwigoURL'];
+		$piwigoParams[ 'wgPiwigoGalleryLayout' ] = $GLOBALS['wgPiwigoGalleryLayout'] ?? 'fluid';
+
+		$parser->getOutput()->addJsConfigVars( 'Piwigo', $piwigoParams );
+
+		$ret = self::getGalleryTag($parameters);
+
+		return $ret;
+	}
+
+	private static function getGalleryTag($parameters)
+	{
+		// Add some synonymes for robustness:
+		if (!empty($parameters['images']))
+			$parameters['count'] = $parameters['images'];
+		unset($parameters['images']);
+
+		if (!empty($parameters['tag']))
+			$parameters['tags'] = $parameters['tag'];
+		unset($parameters['tag']);
+
+		if (!empty($parameters['album']))
+			$parameters['category'] = $parameters['album'];
+		unset($parameters['album']);
+
+		$data = array();
+		foreach ($parameters as $k => $v)
+		{
+			if (is_array($v))
+				$data[] = 'data-'.$k.'_multiple="'.htmlspecialchars(implode(',', $v)).'"';
+			else
+				$data[] = 'data-'.$k.'="'.htmlspecialchars($v).'"';
+		}
+
+		return '<div class="showPiwigo tz-gallery" '.implode(' ', $data).'></div>';
 	}
 }
